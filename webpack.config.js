b703745076cf042d59c7b24e07d104efe86aea32
Andrey.Rymaruk <andrey.rymaruk@busmakret.ua>
2019-04-24 11:42:52 +0300

    Edited structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 const UglifyEsPlugin = require('uglify-es-webpack-plugin');
 
@@ -32,38 +31,3 @@ module.exports = {
     })
   ]
 };
-=======
-const path = require('path');
-const UglifyEsPlugin = require('uglify-es-webpack-plugin');
-
-module.exports = {
-  target: 'node',
-
-  entry: './lib/bmapi.js',
-  output: {
-    filename: 'bmapi.js',
-    library: 'BmApi',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: "umd"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          babelrc: true
-        }
-      }
-    ]
-  },
-  plugins: [
-    new UglifyEsPlugin({
-      compress:{
-        drop_console: true
-      }
-    })
-  ]
-};
->>>>>>> d28ae96296b35dd379fcaf81c4b12d71d6c2df27
