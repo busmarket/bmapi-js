5f8200943ea0b7cf60c2198223aa76c6c1f05580
Andrii Rymaruk <andrewrymaruk@gmail.com>
2020-03-19 12:16:46 +0200

    Edited webpack condig. Edited claims methods

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
 const UglifyEsPlugin = require('uglify-es-webpack-plugin');
 
-module.exports = {
-  target: 'node',
+module.exports = env => { 
+  const isNode = env && env.target && env.target === 'node';
+
+  return {
+  ...(isNode ? {target: 'node'} : null),  
+
+  node: { global: true, fs: 'empty' },
 
   entry: './lib/bmapi.js',
   output: {
-    filename: 'bmapi.js',
+    libraryTarget: 'umd',
+    filename: isNode ? `bmapi.js` : `cdn.bmapi.js`,
     library: 'BmApi',
     path: path.resolve(__dirname, 'dist'),
-    libraryTarget: "umd"
   },
+  
   module: {
     rules: [
       {
@@ -30,4 +36,4 @@ module.exports = {
       }
     })
   ]
-};
+}};
